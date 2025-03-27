b1d9323f9385412887e14c5f0638bf0c1e7a2c45
Tako Schotanus <tako@codejive.org>
2025-03-27 11:16:32 +0100

    chore: made jbang example less modern and added new

--- samples/listinstalled.java
+++ samples/listinstalled.java
@@ -1,10 +1,9 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS dev.jbang:devkitman:0.1.2
 
-class getorinstall {
+class listinstalled {
 	public static void main(String[] args) {
 		JdkManager jdkManager = JdkManager.create();
-		Jdk jdk = jdkManager.getOrInstallJdk(args.length > 0 ? args[0] : "11+");
-		System.out.println("JDK: " + jdk);
+		System.out.println("Installed: " + jdkManager.listInstalledJdks());
 	}
 }
