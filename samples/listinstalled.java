6a612d6f569534ce79c6a5ccb9665d572d376183
Tako Schotanus <tako@codejive.org>
2025-04-02 18:09:56 +0200

    fix: samples were missing imports

--- samples/listinstalled.java
+++ samples/listinstalled.java
@@ -1,6 +1,8 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS dev.jbang:devkitman:0.1.4
 
+import dev.jbang.devkitman.*;
+
 class listinstalled {
 	public static void main(String[] args) {
 		JdkManager jdkManager = JdkManager.create();
