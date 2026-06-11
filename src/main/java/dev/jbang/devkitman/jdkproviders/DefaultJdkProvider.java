0a5e4ff78be15f69e9c9fa3d1ae8a5feb83ace36
Max Rydahl Andersen <max@xam.dk>
2026-06-11 20:44:10 +0200

    Revert "fix: resolve path canonicalization issue in acceptFolder on macOS/Windows"
    This reverts commit 03d814eae7ea986d9bc3d7ac868fadb7deadc940.

--- src/main/java/dev/jbang/devkitman/jdkproviders/DefaultJdkProvider.java
+++ src/main/java/dev/jbang/devkitman/jdkproviders/DefaultJdkProvider.java
@@ -158,7 +158,7 @@ public class DefaultJdkProvider extends BaseFoldersJdkProvider {
 	}
 
 	protected boolean acceptFolder(@NonNull Path jdkFolder) {
-		if (!jdkFolder.equals(defaultJdkLink) && !isUnderRoot(jdkFolder)) {
+		if (!jdkFolder.equals(defaultJdkLink) && !jdkFolder.startsWith(jdksRoot)) {
 			return false;
 		}
 		String nm = jdkFolder.getFileName().toString();
